d5d1ed7ba2e72b9b92e04206d60027e47eda7d52
Ju-unn <s4538@naver.com>
2025-08-16 17:58:43 +0900

    first commit

---
+++ app/src/main/java/com/example/honbab2/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.honbab2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private CardView bottomDetailSheet;
+    private TextView sheetTitle;
+    private ImageView btnCloseDetail;
+    private RecyclerView rvDetailMeetings;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_home);
+
+
+
+//        TextView registerpage = findViewById(R.id.tv_singup_link);
+//
+//        registerpage.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, register.class);
+//                startActivity(intent);
+//            }
+//        });
+
+    }
+
+
+}
